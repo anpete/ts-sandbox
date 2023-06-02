f17b90cc6a32eb119c830af4c794b24eaa2e2a8a
Andrew Peters <anpete@Andrews-MBP.lan>
2023-06-02 15:54:31 +0200

    Enable vitest

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "hello world!"
+pnpm add -D vitest
+npm run test
---
+++ index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("test", () => {
+  it("hello", () => {
+    expect(Math.sqrt(4)).toBe(2);
+  });
+});
